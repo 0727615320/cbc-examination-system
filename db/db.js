d77357405a0f9a97e721f8d8bfcf5cf82ec852d1
0727615320 <josphatmukhwana5@gmail.com>
2025-07-29 13:24:04 +0300

    🌐 Use environment variables and conditional SSL for DB

--- db/db.js
+++ db/db.js
@@ -1,15 +1,14 @@
-const fs = require('fs');
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const sslOptions = process.env.DB_SSL === 'true' ? {
   ssl: {
-    ca: fs.readFileSync('./certs/ca.pem')
+    ca: process.env.MYSQL_CA_PEM
   }
-});
+} : {};
 
-module.exports = pool.promise();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ...sslOptions
+});
