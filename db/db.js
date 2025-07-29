9ec37ccc8b5033b80972d20b6039b3083a42aaf9
0727615320 <josphatmukhwana5@gmail.com>
2025-07-29 09:29:07 +0300

    🔒 Secure db.js by using environment variables instead of hardcoded credentials

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
-// db/db.js
-const mysql = require('mysql2/promise'); // ✅ PROMISE VERSION
+const fs = require('fs');
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Confidence2030',  // ✅ Your actual MySQL password
-  database: 'cbc_mis'
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync('./certs/ca.pem')
+  }
 });
 
-module.exports = db;
+module.exports = pool.promise();
